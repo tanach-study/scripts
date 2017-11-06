82b31d9737e401cb55b3b9cfdf9d35ad242b381d
Joey Pinhas <jpinhas97@yahoo.com>
2017-11-06 11:45:01 -0500

    Attempts to get the lua requests module working

--- analyticsLua/index.lua
+++ analyticsLua/index.lua
@@ -1,1 +1,7 @@
-print("Hello World")
+requests = require('requests')
+response = requests.get('http://httpbin.org/get')
+print(response.status_code)
+
+response = requests.post{url = 'http://httpbin.org/post', data='random data'}
+json_data = response.json()
+print(json_data.data)
