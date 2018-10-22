d325f8874e7a5d966a7f798688652577c09f6d95
Joey Pinhas <jpinhas97@yahoo.com>
2018-10-22 02:14:26 -0400

    Add new for loop syntax

--- bulkDownload/sefer.sh
+++ bulkDownload/sefer.sh
@@ -9,4 +9,13 @@ for i in {1..24}; do
 done
 
 
+typeset -i i lower upper # set these vars to be integer types
+lower=1
+upper=24
+for ((i=lower;i<=upper;i++)); do 
+  url="$base/$part/$directory/$filename$i.mp3"
+  wget $url -P "./test"
+done
+
+
 curl -O https://cdn.tanachstudy.com/archives/Neviim%20Rishonim/Yehoshua/yehoshua-[1-24].mp3
