7f3aff64b1a715c3054b38c15a293a7becd6368b
Joey Pinhas <jpinhas97@gmail.com>
2019-07-16 08:55:05 -0400

    Add utility functions to get more info about mishnayot data

--- readAudioFiles/main.py
+++ readAudioFiles/main.py
@@ -2,6 +2,7 @@ import os
 import csv
 import operator
 from dotenv import load_dotenv
+from mishna_utils import get_seder_from_masechet
 
 load_dotenv('.env')
 FILE_PATH = os.getenv('FILE_PATH')
@@ -14,16 +15,17 @@ for file in onlyfiles:
     replaced = name.replace('-', '_')
     parts = name.split('_')
     masechet = str(parts[0])
+    seder = get_seder_from_masechet(masechet, True)
     perek = str(parts[1])
     mishna = str(parts[2])
     title = ' '.join(parts[3:])
     pretty_title = title.replace('_', ' ')
-    mishnayot.append([masechet, perek, mishna, pretty_title])
+    mishnayot.append([seder, masechet, perek, mishna, pretty_title])
 
 mishnayot.sort(key = operator.itemgetter(1, 2))
 
 with open('titles.csv', 'w') as outfile:
     writer = csv.writer(outfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    writer.writerow(['Masechet', 'Perek', 'Mishna', 'Title'])
+    writer.writerow(['Seder', 'Masechet', 'Perek', 'Mishna', 'Title'])
     for row in mishnayot:
         writer.writerow(row)
