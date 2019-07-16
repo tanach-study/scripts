0ab666ebe3c3e7856d8f5652fc440469e88b8246
Joey Pinhas <jpinhas97@gmail.com>
2019-07-16 09:07:30 -0400

    Restructure the module to be more modular, capable of being imported from

--- handleMishnaFiles/add_mishna_titles.py
+++ handleMishnaFiles/add_mishna_titles.py
@@ -3,48 +3,51 @@ import pandas as pd
 from dotenv import load_dotenv
 from pymongo import MongoClient, UpdateOne
 
-load_dotenv('.env')
-DB_STRING = os.getenv("DB_STRING")
+def create_update_array(files=[]):
+    updates = []
 
-client = MongoClient(DB_STRING)
-db = client.ts
-coll = db.newPerakim
+    for file in files:
+        if os.path.isfile(file):
+            rows = pd.read_csv(file, na_filter=False)
+            for i, row in rows.iterrows():
+                s = row['Seder'].lower()
+                m = row['Masechet'].lower().replace(' ', '-')
+                per = int(row['Perek'])
+                mish = int(row['Mishna'])
+                title = row['Title']
+                if title != "":
+                    updates.append(UpdateOne({ 
+                        "division": "mishna",
+                        "segment": s,
+                        "section": m,
+                        "unit": per,
+                        "part": mish,
+                    }, { "$set": { "part_title": title }} ))
+    return updates
 
-# zeraim = pd.read_csv("./zeraim.csv", na_filter=False)
-# moed = pd.read_csv("./moed.csv", na_filter=False)
-# nashim = pd.read_csv("./nashim.csv", na_filter=False)
-# nezikin = pd.read_csv("./nezikin.csv", na_filter=False)
-# kadashim = pd.read_csv("./kadashim.csv", na_filter=False)
-# taharot = pd.read_csv("./taharot.csv", na_filter=False)
+def perform_updates(updates=[]):
+    load_dotenv('.env')
+    DB_STRING = os.getenv('DB_STRING')
 
-files = [
-    "./zeraim.csv",
-    "./moed.csv",
-    "./nashim.csv",
-    "./nezikin.csv",
-    "./kadashim.csv",
-    "./taharot.csv",
-]
+    client = MongoClient(DB_STRING)
+    db = client.ts
+    coll = db.newPerakim
+    result = coll.bulk_write(updates)
+    return result
 
-updates = []
+def run(files):
+    updates = create_update_array(files)
+    result = perform_updates(updates)
+    return result
 
-for file in files:
-    if os.path.isfile(file):
-        rows = pd.read_csv(file, na_filter=False)
-        for i, row in rows.iterrows():
-            s = row['Seder'].lower()
-            m = row['Masechet'].lower().replace(' ', '-')
-            per = int(row['Perek'])
-            mish = int(row['Mishna'])
-            title = row['Title']
-            if title != "":
-                updates.append(UpdateOne({ 
-                    "division": "mishna",
-                    "segment": s,
-                    "section": m,
-                    "unit": per,
-                    "part": mish,
-                }, { "$set": { "part_title": title }} ))
-
-result = coll.bulk_write(updates)
-print result
+if __name__ == '__main__':
+    files = [
+        "./zeraim.csv",
+        "./moed.csv",
+        "./nashim.csv",
+        "./nezikin.csv",
+        "./kadashim.csv",
+        "./taharot.csv",
+    ]
+    result = run(files)
+    print result
