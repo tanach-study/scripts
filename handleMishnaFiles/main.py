7108214f85111e184ef807709097c08bf0b56afa
Joey Pinhas <jpinhas97@gmail.com>
2019-07-16 09:22:30 -0400

    Add script that utilizes both modules and does everything in one step (UNTESTED)

---
+++ handleMishnaFiles/main.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+from read_audio_files import get_mishnayot_from_files_at_path, write_mishnayot_to_csv_file
+from add_mishna_titles.py import run as add_mishna_titles
+
+def run():
+    load_dotenv('.env')
+    FILE_PATH = os.getenv('FILE_PATH')
+
+    mishnayot = get_mishnayot_from_files_at_path(FILE_PATH)
+    file_name = './titles.csv'
+    write_mishnayot_to_csv_file(mishnayot, file_name)
+    
+    files = [file_name]
+    result = add_mishna_titles(files)
+
+    print(result)
+
+if __name__ == '__main__':
+    run()
