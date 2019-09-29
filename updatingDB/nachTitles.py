cff003310ec11a48f7ce834d6cc9c3c1f5de2528
Joey Pinhas <jpinhas97@gmail.com>
2019-09-29 13:13:49 -0400

    Update script and seeds

--- updatingDB/nachTitles.py
+++ updatingDB/nachTitles.py
@@ -7,14 +7,12 @@ load_dotenv('.env')
 DB_STRING = os.getenv("DB_STRING")
 
 client = MongoClient(DB_STRING)
-db = client.ts
-perakim_collection = db.perakim
-sefarim_collection = db.books
+db = client["ts-prod"]
+perakim_collection = db["newPerakim"]
 
 perakim = pd.read_csv("./perakim.csv", na_filter=False)
 
 update_perakim_list = []
-update_sefarim_list = []
 
 for i, row in perakim.iterrows():
     s = row['Sefer'].lower()
@@ -22,12 +20,9 @@ for i, row in perakim.iterrows():
     title = row['Title']
     if p.isdigit():
         if title != "":
-            update_perakim_list.append(UpdateOne({ "book_name": s, "perek_id": int(p) }, { "$set": { "perek_title": title }} ))
-            update_sefarim_list.append(UpdateOne({ "seferMeta.book_name": s, "allPerakim.perek_id": int(p) }, { "$set": { "allPerakim.$.perek_title": title }} ))
+            update_perakim_list.append(UpdateOne({ "section": s, "unit": p }, { "$set": { "unit_title": title }} ))
     else:
         print s, p, title, "FALSE"
 
 perakim_result = perakim_collection.bulk_write(update_perakim_list)
-sefarim_result = sefarim_collection.bulk_write(update_sefarim_list)
 print perakim_result.bulk_api_result
-print sefarim_result.bulk_api_result
